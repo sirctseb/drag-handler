0089273e94de563f68a212c67cfb4063ec479aed
Christopher Best <bestchris@gmail.com>
2012-12-03 23:14:09 -0500

    refactored drag handler a little more nicely

--- lib/drag_handler.dart
+++ lib/drag_handler.dart
@@ -1,15 +1,22 @@
 part of Tabasci;
 
+// TODO this should probably be a singleton. when do you drag more than one thing at a time?
 class DragHandler {
   
-  // the function to be called on mouse move when dragging
-  var _dragCallback;
+  /// The function to be called on mouse move when dragging
+  var dragCallback;
   
-  // the mouse up handler
+  /// The function to be called when dragging begins
+  var startCallback;
+  
+  /// The function to be called when dragging ends
+  var endCallback;
+  
+  // reference to the local mouse up handler
   var _mouseUp;
   
-  // true if we should automatically stop drags on mouse up
   bool _autoStop;
+  /// True if we should automatically stop drags on mouse up
   bool get autoStop => _autoStop;
   set autoStop(bool a) {
     if(_autoStop != a) {
@@ -23,32 +30,38 @@ class DragHandler {
       }
     }
   }
-    
+  
   DragHandler() {
-    _mouseUp = autoStopUpHandler;
+    // store a reference to the autostop up handler so we can add and remove it
+    _mouseUp = _autoStopUpHandler;
+    // set autostop on by default
     autoStop = true;
   }
   
-  void autoStopUpHandler(MouseEvent event) {
+  // the method that will be called on mouse up events when autostop is on
+  void _autoStopUpHandler(MouseEvent event) {
     print("drag handler: got mouse up, stopping drag");
-    // stop drag
+    // stop the drag
     stopDrag();
   }
   
-  void startDrag(callback) {
+  /// Begin the drag
+  void startDrag() {
     print("drag handler: registering call back and move event");
     // if there is an existing callback, remove it
     stopDrag();
-    // store the callback
-    _dragCallback = callback;
     // register for the move event
-    document.body.on.mouseMove.add(_dragCallback);
+    if(dragCallback != null) {
+      document.body.on.mouseMove.add(dragCallback);
+    }
   }
+  
+  /// Manually end the drag
   void stopDrag() {
     print("drag handler: removing callback");
     // remove the callback
-    if(_dragCallback != null) {
-      document.body.on.mouseMove.remove(_dragCallback);
+    if(dragCallback != null) {
+      document.body.on.mouseMove.remove(dragCallback);
     }
   }
 }
