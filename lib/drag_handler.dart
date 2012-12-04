c6d78b4e35ff3b6eb4e2c816cd85841fcbbd8a5a
Christopher Best <bestchris@gmail.com>
2012-12-03 22:33:04 -0500

    fixed order of initialization

--- lib/drag_handler.dart
+++ lib/drag_handler.dart
@@ -25,16 +25,18 @@ class DragHandler {
   }
     
   DragHandler() {
-    autoStop = true;
     _mouseUp = autoStopUpHandler;
+    autoStop = true;
   }
   
   void autoStopUpHandler(MouseEvent event) {
+    print("drag handler: got mouse up, stopping drag");
     // stop drag
     stopDrag();
   }
   
   void startDrag(callback) {
+    print("drag handler: registering call back and move event");
     // if there is an existing callback, remove it
     stopDrag();
     // store the callback
@@ -43,6 +45,7 @@ class DragHandler {
     document.body.on.mouseMove.add(_dragCallback);
   }
   void stopDrag() {
+    print("drag handler: removing callback");
     // remove the callback
     if(_dragCallback != null) {
       document.body.on.mouseMove.remove(_dragCallback);
